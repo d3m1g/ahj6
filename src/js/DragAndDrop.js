3dd89e31d075a046ab5cbe582198e93ca3d6b5da
d3m1g <116607635+d3m1g@users.noreply.github.com>
2024-06-16 03:43:49 +1000

    Update DragAndDrop.js

--- src/js/DragAndDrop.js
+++ src/js/DragAndDrop.js
@@ -1,16 +1,16 @@
 /* eslint-disable eol-last */
 /* eslint-disable indent */
 export default function DragAndDrop(event, element) {
- const closest = document.elementFromPoint(event.clientX, event.clientY);
- const { top } = closest.getBoundingClientRect();
+  const closest = document.elementFromPoint(event.clientX, event.clientY);
+  const { top } = closest.getBoundingClientRect();
 
- if (closest.classList.contains('list_item')) {
-   if (event.pageY > window.scrollY + top + closest.offsetHeight / 2) {
-     closest.closest('.task_list').insertBefore(element, closest.nextElementSibling);
+  if (closest.classList.contains('list_item')) {
+    if (event.pageY > window.scrollY + top + closest.offsetHeight / 2) {
+      closest.closest('.task_list').insertBefore(element, closest.nextElementSibling);
    } else {
-     closest.closest('.task_list').insertBefore(element, closest);
+      closest.closest('.task_list').insertBefore(element, closest);
    }
- } else if (closest.classList.contains('task_list')) {
-   closest.append(element);
+ }   else if (closest.classList.contains('task_list')) {
+       closest.append(element);
  }
 }
